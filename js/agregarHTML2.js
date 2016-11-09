4634c85dd03b076283f4b7838e13e9307a3b8469
Crisamer Ortega González <crisamer.ortega@gmail.com>
2016-11-09 16:28:54 +0000

    agregar HTML2

--- js/agregarHTML2.js
+++ js/agregarHTML2.js
@@ -1,7 +1,18 @@
 $("#boton3").click(function(){
 
-	//Agregar a inicio de contenedor
+	//Agregar al contenedor
 	$("#contenedor2").append('<div clas="text">Texto del "append"</div>')
 
-	$("#")
+	//Agrega antes de "c1"
+	$("#c1").before('<p>Texto de "before"</p>')
+
+	//Agrega despues de "c1"
+	$("#c1").after('<p>Texto de "after"</p>')
+
+	//Agrega en "c1"
+	$("#c1").prepend('Texto de "prepend" ')
+
+	//Cambiar texto de c3
+	$("#c1").test('Texto de reemplazo ')	
+	
 });
